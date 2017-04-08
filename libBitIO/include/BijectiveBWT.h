0afbac751fdda5ebffd812f1758a4f9a14c750ad
Marcus Johnson <bumblebritches57@gmail.com>
2017-04-08 17:22:34 -0400

    Added BijectiveBWT files

---
+++ libBitIO/include/BijectiveBWT.h
@@ -0,0 +1,16 @@
+#include "../include/BitIO.h"
+
+#pragma once
+
+#ifndef LIBBITIO_BIJECTIVEBWT_H
+#define LIBBITIO_BIJECTIVEBWT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LIBBITIO_BIJECTIVEBWT_H */
---
+++ libBitIO/src/BijectiveBWT.c
@@ -0,0 +1,30 @@
+#include "../include/BijectiveBWT.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+    void EncodeBijectiveBWT(uint16_t ****Array, size_t ArraySize) {
+        
+    }
+    
+    void RotateArray(const size_t DataSize, int64_t *Data, const uint64_t NumRotations, const bool RotateRight) {
+        // Theoretical speed up: just edit the Pointer to each array element, and increment or decrement it by  NumRotation.
+        if (RotateRight == true) {
+            for (uint64_t Rotation = 0; Rotation < NumRotations; Rotation++) {
+                for (uint64_t Element = 0; Element < DataSize; Element++) {
+                    Data[Element + 1] = Data[Element]; // TODO: Make sure the last element wraps around to the end.
+                }
+            }
+        } else { // Rotate left
+            for (uint64_t Rotation = 0; Rotation < NumRotations; Rotation++) {
+                for (uint64_t Element = DataSize; Element > 0; Element--) {
+                    Data[Element] = Data[Element - 1];
+                }
+            }
+        }
+    }
+    
+#ifdef __cplusplus
+}
+#endif
